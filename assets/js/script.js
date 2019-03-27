1ec5f7799409fbc557b0671eb7ee8292f511e5ee
AmableRivas <amablerivas@gmail.com>
2019-03-27 06:26:44 -0300

    10.Incorporación del smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
+$(function(){
+  	
+  	$('[data-toggle="tooltip"]').tooltip();
+
+	$("a").click(function(event){
+		if(this.hash !== "") {
+			event.preventDefault();
+
+			var gato = this.hash;
+
+			$("html, body").animate({
+				scrollTop: $(gato).offset().top-150
+			}, 1500, function(){
+				window.location.hash = gato;
+			});
+		}
+		//Pregunta: Como hacer que el offset funcione bien
+	});
+
+
+	$('[data-toggle="popover"]').popover();
 });
